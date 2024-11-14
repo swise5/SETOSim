ce8104db26f3a2245d086b8edafcee4d53ada5df
Sarah Wise <swise0@gmail.com>
2024-11-14 17:42:02 +0000

    vehicle improvements and testing

--- src/test/java/uk/ac/ucl/SETOSim/myobjects/VehicleTests.java
+++ src/test/java/uk/ac/ucl/SETOSim/myobjects/VehicleTests.java
@@ -13,6 +13,8 @@ import sim.field.geo.GeomVectorField;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 public class VehicleTests {
 
 	public static String testingDirectory = "data/testing/";
@@ -33,21 +35,55 @@ public class VehicleTests {
 	}
 	
 	@Test
-	public void OperatorTakesTheWheel() {
+	public void OperatorsAndOccupantsCanEnterAndLeaveTheVehicle() {
 		
-		//this is too complicated, apparently - I need to start from an even simpler, less entangled point
-		
-		// assign
-			TakamatsuSim world = setupTestingWorld(this.testingDirectory, 0);		
-			Person p = setupTestingPerson(world);
-			Vehicle sut = new Vehicle(null, null, 0, null);
+		// ASSIGN -
+		TakamatsuSim world = setupTestingWorld(this.testingDirectory, 0);		
+		Person p = setupTestingPerson(world);
+		Person passenger = setupTestingPerson(world);
+		Vehicle sut = new Vehicle(null, null, 1, null);
 			
-			// act
-			sut.setOperator(p);
+		// ACT ---
+		// add the operator
+		sut.setOperator(p);
+		Person operator = sut.getOperator();
+		int numInitialPassengers = sut.getPassengers().size();
+		boolean initialOccupied = sut.hasOccupants();
+		
+		// add a passenger
+		sut.addPassenger(passenger);
+		int numPassengersWhenOneAdded = sut.getPassengers().size();
+		boolean occupiedByPassenger = sut.hasOccupants();
+		
+		// take the operator out and confirm
+		sut.removeOperator();
+		boolean occupiedOnlyByPassenger = sut.hasOccupants();
+		
+		// now remove the passenger
+		ArrayList <Person> formerOccupants = sut.removeAllPassengers();
+		int numFinalPassengers = sut.getPassengers().size();
+		boolean occupiedAfterAllLeft = sut.hasOccupants();
 
-			//assert
-			assertEquals(p, sut.getOperator()); // checking that someone is in there
-			assertEquals(1, sut.getPassengers().size()); // no passengers
+		
+		// ASSERT - 
+		
+		// operator tests
+		assertEquals(p, operator); // checking that someone is in there
+		assertEquals(0, numInitialPassengers); // no passengers added yet
+		assertTrue(initialOccupied); // there are no passengers, but there's someone in the vehicle
+		
+		// passenger tests
+		assertEquals(1, numPassengersWhenOneAdded); // when passenger added, should be 1 person - operator is separate!
+		assertTrue(occupiedByPassenger); // they're both in there
+		
+		// operator removed, passenger still there
+		assertTrue(occupiedOnlyByPassenger); // even though the operator has left, the vehicle is still occupied!
+		
+		// passenger removed
+		assertTrue(formerOccupants.contains(passenger)); // passenger is in the group removed from the vehicle
+		assertEquals(0, numFinalPassengers); // passenger list is now reset to be empty
+		assertFalse(occupiedAfterAllLeft); // there shouldn't be anyone left there
 	}
+	
 	
 }
